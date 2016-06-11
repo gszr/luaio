543da78f9011a174bff98784b56b510db077d1a3
Guilherme Salazar <gmesalazar@gmail.com>
2016-06-11 18:39:45 -0300

    add bind, connect, read & write

--- staging/sockets/luasockets.c
+++ staging/sockets/luasockets.c
@@ -3,21 +3,29 @@
 #include <sys/lua.h>
 #include <sys/module.h>
 #include <sys/socketvar.h>
+#include <netinet/in.h>
 
 #include <lua.h>
 #include <lauxlib.h>
-#include <mappings.h>
+
+#include <stdio.h>
 
 MODULE(MODULE_CLASS_MISC, luasockets, "lua");
+
+#define BUF_SIZE 1024
+
+struct luasocket {
+	struct socket *socket;
+	int fd;
+};
 
 static int
 sockets_socket(lua_State *L)
 {
+	struct luasocket *luasocket;
 	struct socket *so;
-	
 	const char *domain;
 	const char *type;
-
 	int sfd;
 	int err;
 	int dom = 0;
@@ -26,7 +34,8 @@ sockets_socket(lua_State *L)
 	domain = luaL_checkstring(L, 1);
 	type   = luaL_checkstring(L, 2);
 
-	if (strcmp(domain, "inet") == 0 || strcmp(domain, "inet4"))
+	//XXX not about using strings to name domains (types, etc)
+	if (strcmp(domain, "inet4") == 0)
 		dom = AF_INET;
 	else if (strcmp(domain, "inet6"))
 		dom = AF_INET6;
@@ -45,20 +54,118 @@ sockets_socket(lua_State *L)
 	if ((err = fsocreate(dom, &so, typ, 0, &sfd)))
 		luaL_error(L, "could not create socket (error %d)", err);
 
-	lua_pushlightuserdata(L, so);
+	luasocket = (struct luasocket*)
+		lua_newuserdata(L, sizeof(struct luasocket));
+	luasocket->socket = so;
+	luasocket->fd = sfd;
+
 	return 1;
 }
 
 static int
 sockets_close(lua_State *L)
 {
+	struct luasocket *luasocket;
 	int err;
-	struct socket *so;
-	if ((so = (struct socket *) lua_touserdata(L, 1)))
-		luaL_error(L, "invalid socket\n");
-	if ((err = soclose(so)))
-		luaL_error(L, "could inot close socket (error %d)\n", err);
+
+	if ((luasocket = (struct luasocket *) lua_touserdata(L, 1)) == NULL)
+		luaL_error(L, "invalid Lua socket\n");
+	if ((err = soclose(luasocket->socket)) != 0)
+		luaL_error(L, "could not close socket (error %d)\n", err);
 	lua_pushboolean(L, 1);
+	return 1;
+}
+
+typedef int BindOrConnect(struct socket*, struct sockaddr*, struct lwp*);
+
+static int
+sockets_bind_connect(BindOrConnect handler, lua_State *L)
+{
+	struct luasocket *luasocket;
+	const char *domn;
+	const char *addr;
+	int err;
+
+	if ((luasocket = (struct luasocket *) lua_touserdata(L, 1)) == NULL)
+		luaL_error(L, "invalid socket");
+	domn = luaL_checkstring(L, 2);
+	addr = luaL_checkstring(L, 3);
+
+	// XXX inet4 and inet6 could share code
+	// XXX we could also declare a sockaddr in the function scope
+	// and then specialize it to sockaddr_in(,6, un) in the branches below
+	if (strcmp(domn, "inet4") == 0) {
+		struct sockaddr_in in4addr;
+
+		if (lua_gettop(L) != 4)
+			luaL_error(L, "inet4 expects an IP address and a port number");
+
+		memset(&in4addr, 0, sizeof(in4addr));
+		in4addr.sin_len = sizeof(struct sockaddr_in);
+		in4addr.sin_addr.s_addr = inet_addr(addr);
+		in4addr.sin_port = htons(luaL_checkinteger(L, -1));
+		in4addr.sin_family = AF_INET;
+
+		solock(luasocket->socket);
+		if ((err = handler(luasocket->socket, (struct sockaddr*) &in4addr,
+			curlwp)) != 0)
+			luaL_error(L, "could not connect");
+		sounlock(luasocket->socket);
+
+		lua_pushboolean(L, 1);
+	} else if (strcmp(domn, "local") == 0) {
+		;
+	}
+
+	return 1;
+}
+
+static int
+sockets_connect(lua_State *L)
+{
+	return sockets_bind_connect(soconnect, L);
+}
+
+static int
+sockets_bind(lua_State *L)
+{
+	return sockets_bind_connect(sobind, L);
+}
+
+static int
+sockets_write(lua_State *L)
+{
+	struct luasocket* luasocket;
+	const char *buff;
+	int cnt;
+
+	if ((luasocket = (struct luasocket*) lua_touserdata(L, 1)) == NULL)
+		luaL_error(L, "invalid socket");
+	buff = luaL_checkstring(L, 2);
+
+	//XXX handle binary data
+	cnt = write(luasocket->fd, buff, strlen(buff));
+	lua_pushinteger(L, cnt);
+
+	return 1;
+}
+
+static int
+sockets_read(lua_State *L)
+{
+	struct luasocket* luasocket;
+	//XXX probably not a good idea; lua_pushlstring?
+	char buff[BUF_SIZE];
+	int cnt;
+
+	if ((luasocket = (struct luasocket*) lua_touserdata(L, 1)) == NULL)
+		luaL_error(L, "invalid socket");
+
+	cnt = read(luasocket->fd, &buff, BUF_SIZE);
+
+	lua_pushinteger(L, cnt);
+	lua_pushlstring(L, buff, cnt);
+
 	return 1;
 }
 
@@ -67,7 +174,11 @@ luaopen_sockets(lua_State *L)
 {
 	const luaL_Reg sockets_lib[] = {
 		{"socket", sockets_socket},
+		{"bind", sockets_bind},
+		{"connect", sockets_connect},
 		{"close", sockets_close},
+		{"write", sockets_write},
+		{"read", sockets_read},
 		{ NULL, NULL }
 	};
 
